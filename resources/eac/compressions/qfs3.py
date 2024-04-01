893116844fb217191ab999e3fd53b0b12b61fb39
Jon Tandy <jontandyuk@hotmail.com>
2024-04-01 23:25:32 +0100

    Revert to original uncompress function for consistency with original repository

--- resources/eac/compressions/qfs3.py
+++ resources/eac/compressions/qfs3.py
@@ -117,78 +117,77 @@ class Qfs3Compression(BaseCompressionAlgorithm, AsmRunner):
     def uncompress(self, buffer: BufferedReader, input_length: int) -> bytes:
         uncompressed: bytearray = bytearray()
 
-        table_110 = [0] * 16                                        # number of entries of x bits long
+        table_110 = [0] * 16
 
         self.define_variable('var_14C', -0x14C, 4)
         self.define_variable('var_154', -0x154, 4)
 
-        index_table_0_capacity = 0                                  # this is the sum of all the values in table_110
+        index_table_0_capacity = 0
         unk_counter = 0
 
         self.available_acc_bits = 0
-        file_header = self.accumulator = read_short(buffer, 'big') # 2b read - read the header
-        self.read_next(buffer)                                     # 2b read - read the output size (little-endian)
-        self.esi = self.accumulator << 16                          # insert stream into esi, minus header
-        # if compressed size presented                             -- stock TNFS PBS are not compressed (0x30FB) so this will not happen
-        if file_header & 0x100:                                    
+        file_header = self.accumulator = read_short(buffer, 'big')
+        self.read_next(buffer)
+        self.esi = self.accumulator << 16
+        # if compressed size presented
+        if file_header & 0x100:
             self.edx = read_int(buffer, 'big')
             self.available_acc_bits = 8
             self.esi = self.edx << 8
             self.accumulator = self.edx
             # reset size presented flag bit
             file_header = file_header & 0xFEFF
-        self.ebx = self.esi >> 0x18                                # ebx will be little-endian value of size = 000D
-        self.available_acc_bits -= 8                               # cancel compressed value if compressed
-        self.esi = self.esi << 8                                   # step through 1 byte
-        self.accumulate_if_needed(buffer)                          # this will read unless compressed - so for TNFS, yes
-        self.eax = self.esi >> 16                                  # esi should be blank so = 0000
-        self.available_acc_bits -= 16                              # should be 16 by now, so = 0
-        self.esi = self.esi << 16                                  # esi should be blank so = 0000
-        self.output_length = self.eax                              # Set output length.  eax = first 3 bytes = length header in file
+        self.ebx = self.esi >> 0x18
+        self.available_acc_bits -= 8
+        self.esi = self.esi << 8
         self.accumulate_if_needed(buffer)
-        self.edx = self.output_length = self.output_length | (self.ebx << 16) # size is a 3-byte value. Use bitmask to adjust.
+        self.eax = self.esi >> 16
+        self.available_acc_bits -= 16
+        self.esi = self.esi << 16
+        self.output_length = self.eax
+        self.accumulate_if_needed(buffer)
+        self.edx = self.output_length = self.output_length | (self.ebx << 16)
         self.ebx = 1
         self.eax = self.esi >> 0x18
         self.available_acc_bits -= 8
         self.esi = self.esi << 8
-        unk_1 = self.al                                     # store for later.  Not used in loop 1 but this equals the sum of values in table_110
+        unk_1 = self.al
         self.accumulate_if_needed(buffer)
-        count_sing = 1
-        val_shift = 15                                          # gives a max of 16 values. Decrements with every iteration of loop 1. Used to bitshift ebp when edx > 0
-        count_quad = 4                                           # 0b100
+        unk_90 = 1
+        unk_3 = 15
+        unk_2 = 4
         self.ebp = 0
-        while True:                                         # Loop 1: this loop appears to be reading the first chunk of data 3 bytes at a time and generating tables from it.  table_110 and unk_table_3 are built.  Some values are set with set_value but not yet clear where/why
-                                                            # eax = length of tree branch? ebx holds counters for inc/dec. edx holds command bits for mem storage. esi = input buffer
-            self.ebp = self.ebp << 1                        # TODO: evaluate evolution of ebp across loops
-            self.ecx = index_table_0_capacity               # set to the sum of all flag bits so far
-            self.eax = self.ebp - self.ecx                  # TODO: work out what this value represents. Could be some kind of mask?
-            self.edx = count_quad                                # unk_2 is always a multiple of 4 as it increments by 4 each run
-            assert self.edx % 4 == 0                        # logic below will work in different way if not
-            self.unk_table_3[int(self.edx / 4)] = self.eax  # store the value above (bp - ecx) in a lookup table. How many we've had vs could have had?
-            if self.get_register_signed_value('esi') >= 0:  # only switch if the leading bit is 0
-                self.eax = 2                                # start eax at 2
-                if (self.esi >> 16) == 0:                   # check we aren't low on data
+        while True:
+            self.ebp = self.ebp << 1
+            self.ecx = index_table_0_capacity
+            self.eax = self.ebp - self.ecx
+            self.edx = unk_2
+            assert self.edx % 4 == 0  # logic below will work in different way if not
+            self.unk_table_3[int(self.edx / 4)] = self.eax
+            if self.get_register_signed_value('esi') >= 0:
+                self.eax = 2
+                if (self.esi >> 16) == 0:
                     self.edx = 0
-                    while self.edx == 0:                    # keep reading until we hit a 1 in our input buffer
+                    while self.edx == 0:
                         self.edx = self.esi >> 0x1F
-                        self.eax += 1                       # count how many zeroes, minimum 2 (eax = 2 initially)
-                        self.available_acc_bits -= 1        # move sub-pointer up 1 bit
+                        self.eax += 1
+                        self.available_acc_bits -= 1
                         self.esi = self.esi << 1
                         self.accumulate_if_needed(buffer)
                 else:
-                    while self.get_register_signed_value('esi') >= 0:   # move 1 bit at a time and count the zeroes until we hit a 1
-                        self.esi = self.esi << 1            # move through inbuf 1 bit at a time until we hit 1
-                        self.eax += 1                       # add 1 to the count for every zero
-                    self.edx = self.eax - 1                 # total zeroes read, minus 1(?)
-                    self.available_acc_bits -= self.edx      # update how much we read
-                    self.esi = self.esi << 1                # move up 1 bit
+                    while self.get_register_signed_value('esi') >= 0:
+                        self.esi = self.esi << 1
+                        self.eax += 1
+                    self.edx = self.eax - 1
+                    self.available_acc_bits -= self.edx
+                    self.esi = self.esi << 1
                     self.accumulate_if_needed(buffer)
-                if self.get_register_signed_value('eax') <= 16: # checking against 2 + number of zeroes in a row.  Why does this need to be signed?
-                    self.edx = self.esi >> (0x20 - self.eax)    # esi shifted by 32 minus the value.
-                    self.ecx = self.al                          # 
-                    self.available_acc_bits -= self.eax         # subtract what we've just processed
-                    self.esi = self.esi << self.cl              # clear the data we just processed from the input stream
-                    self.accumulate_if_needed(buffer)           # read more if we're low
+                if self.get_register_signed_value('eax') <= 16:
+                    self.edx = self.esi >> (0x20 - self.eax)
+                    self.ecx = self.al
+                    self.available_acc_bits -= self.eax
+                    self.esi = self.esi << self.cl
+                    self.accumulate_if_needed(buffer)
                 else:
                     self.ebx = self.eax - 16
                     self.edx = self.esi >> (0x20 - self.ebx)
@@ -201,47 +200,46 @@ class Qfs3Compression(BaseCompressionAlgorithm, AsmRunner):
                     self.esi = self.esi << 16
                     self.accumulate_if_needed(buffer)
                     self.edx = (self.edx << 16) | self.ebx
-                self.edx += (1 << self.al)          # table value = 2^eax. Append it to edx.
-            else:                                   # for values where the if is <=0
-                self.edx = self.esi >> 0x1D         # read the top 3 bits of the buffer (shift 0x1D = 29 bits)
+                self.edx += (1 << self.al)
+            else:
+                self.edx = self.esi >> 0x1D
                 self.available_acc_bits -= 3
-                self.esi = self.esi << 3            # advance the buffer 3 bits
+                self.esi = self.esi << 3
                 self.accumulate_if_needed(buffer)
-            self.edx -= 4                           # Arriving from the normal loops, this will subtract. Arriving from the else, this would cancel bit 0 of the 3-bit buffer.
-            self.eax = count_quad
-            assert self.eax % 4 == 0                # should always be true as unk_2 increments by 4 per run
-            table_110[int(self.eax / 4)] = self.edx # holding the value of edx from this run
-            self.eax = index_table_0_capacity       # retrieve total values in the tree so far
-            self.ebp += self.edx                    # add the value of the 2 bits to the base pointer
-            self.eax += self.edx                    # add the 2 bits to eax
-            self.ecx = 0                            # zero ecx ready for next operation
-            index_table_0_capacity = self.eax       # add the total values counted from this run to the total
-            if self.edx != 0:                       # were there any values recorded this run?
-                self.cl = val_shift & 0xFF              # Bitmask the counter
-                self.eax = self.ebp << self.cl      # shift the bp by ecx bits
-                self.ecx = self.eax & 0xFFFF        # drop the clamped result back into ecx.  Eventually this will overflow.
-            self.ebx = val_shift - 1                # decrement unk_3 but drop it in ebx
-            self.eax = count_quad = count_quad + 4        # increment unk_2 by 4 and drop it in eax
-            val_shift = self.ebx                    # formally decrement unk_3.  Is this a counter of e.g. number of chunks / instructions?
-            self.ebx = count_sing + 1               # increment unk_90 but drop it in ebx
-            self.set_value('[esp+eax+550h+var_154]', self.ecx)  # what is this value? varies run-to-run. ecx is the output - where is it going?
-            count_sing = self.ebx                   # formally increment unk_90
-            if self.edx == 0:                   
-                continue                        # not finished yet - jump to start of loop
-            if self.ecx == 0:                   # check if we've accumulated all values (255) yet
-                break                           # end of reading
-        # Set things up for second loop
+            self.edx -= 4
+            self.eax = unk_2
+            assert self.eax % 4 == 0
+            table_110[int(self.eax / 4)] = self.edx
+            self.eax = index_table_0_capacity
+            self.ebp += self.edx
+            self.eax += self.edx
+            self.ecx = 0
+            index_table_0_capacity = self.eax
+            if self.edx != 0:
+                self.cl = unk_3 & 0xFF
+                self.eax = self.ebp << self.cl
+                self.ecx = self.eax & 0xFFFF
+            self.ebx = unk_3 - 1
+            self.eax = unk_2 = unk_2 + 4
+            unk_3 = self.ebx
+            self.ebx = unk_90 + 1
+            self.set_value('[esp+eax+550h+var_154]', self.ecx)
+            unk_90 = self.ebx
+            if self.edx == 0:
+                continue
+            if self.ecx == 0:
+                break
         self.ecx = 0xFFFFFFFF
-        unk_8 = self.ebx - 1                    # record the final count of steps for later.  Needed for loop 3.
+        unk_8 = self.ebx - 1
         self.set_value('[esp+ebx*4+550h+var_154]', self.ecx)
         self.ebx = 0
         huff_table_0_iter_ptr = 0
         self.eax = 0xFF
         unk_4 = 0
-        if index_table_0_capacity > 0:                          # Loop 2
+        if index_table_0_capacity > 0:
             self.index_table_0 = [None] * index_table_0_capacity
-            while True:                                         # iterate through, populating index_table_0.  Count runs in ebx.  Hold total run count in ecx and break at num limit.
-                if self.get_register_signed_value('esi') >= 0:  # if leading bit = 0
+            while True:
+                if self.get_register_signed_value('esi') >= 0:
                     self.edx = 2
                     if (self.esi >> 16) == 0:
                         while True:
@@ -254,7 +252,7 @@ class Qfs3Compression(BaseCompressionAlgorithm, AsmRunner):
                             if self.ebp != 0:
                                 break
                     else:
-                        while self.get_register_signed_value('esi') >= 0: # keep reading esi bit-by-bit until we hit a one
+                        while self.get_register_signed_value('esi') >= 0:
                             self.esi = self.esi << 1
                             self.edx += 1
                         self.ebx = self.edx - 1
@@ -305,26 +303,26 @@ class Qfs3Compression(BaseCompressionAlgorithm, AsmRunner):
                 huff_table_0_iter_ptr = self.ebx
                 if self.get_register_signed_value('ebx') >= self.get_register_signed_value('ecx'):
                     break
-        self.unk_table_2 = [0x40] * 256 # create a 256-length table, value 64
+        self.unk_table_2 = [0x40] * 256
         self.edx = 0
         self.ebx = 0
         self.ecx = unk_8
         unk_5 = 1
-        unk_9 = 0                       # used in loop 3 - and loop 4 in case of break flag (0x60h / 96d)
-        if self.ecx >= 1:               # should be > 1 as this value came from loop 1
+        unk_9 = 0
+        if self.ecx >= 1:
             self.ecx = 7
             self.eax = 4
-            count_quad = self.ecx
-            val_shift = self.eax
-            while True:                                         # populate unk_table and unk_table_2
-                self.eax = val_shift
+            unk_2 = self.ecx
+            unk_3 = self.eax
+            while True:
+                self.eax = unk_3
                 assert self.eax % 4 == 0
-                self.eax = table_110[int(self.eax / 4)]         # read back in from table_110
+                self.eax = table_110[int(self.eax / 4)]
                 self.ebp = unk_5
                 unk_6 = self.eax
                 if self.get_register_signed_value('ebp') >= 9:
                     break
-                self.ecx = count_quad
+                self.ecx = unk_2
                 self.ebp = 1 << self.cl
                 break_outer = False
                 continue_outer = False
@@ -332,8 +330,8 @@ class Qfs3Compression(BaseCompressionAlgorithm, AsmRunner):
                     self.eax = unk_6 - 1
                     unk_6 = self.eax
                     if self.eax == 0xFFFFFFFF:
-                        self.ebp = val_shift = val_shift + 4
-                        self.eax = count_quad = count_quad - 1
+                        self.ebp = unk_3 = unk_3 + 4
+                        self.eax = unk_2 = unk_2 - 1
                         self.ecx = unk_5 + 1
                         unk_5 = self.ecx
                         self.ebp = unk_8
@@ -369,31 +367,31 @@ class Qfs3Compression(BaseCompressionAlgorithm, AsmRunner):
                 if continue_outer:
                     continue
                 break
-        while True:                                             # write the file out using the lookup tables
+        while True:
             if len(uncompressed) > self.output_length:
                 raise Exception('Uncompress algorythm writes more that file length')
-            self.eax = self.unk_table_2[self.esi >> 0x18]       # should be little-endian - clamp index to 256. Len to read from inbuf / esi
+            self.eax = self.unk_table_2[self.esi >> 0x18]
             self.available_acc_bits -= self.eax
             while self.available_acc_bits >= 0:
-                self.edx = self.esi >> 0x18                     # get 1 byte from front of stream
+                self.edx = self.esi >> 0x18
                 for _ in range(4):
-                    self.append_to_output(uncompressed, self.unk_table[self.edx])   # read value of unk_table from instream lookup and output
+                    self.append_to_output(uncompressed, self.unk_table[self.edx])
                     self.esi = self.esi << self.al
                     self.edx = self.esi >> 0x18
-                    self.eax = self.unk_table_2[self.edx]   # length of value that was read from input
+                    self.eax = self.unk_table_2[self.edx]
                     self.available_acc_bits -= self.eax
                     if self.available_acc_bits < 0:
-                        break                                   # stop and get more input
-            self.available_acc_bits += 0x10                     # add 2 bytes to available acc. bits tally
+                        break
+            self.available_acc_bits += 0x10
             if self.available_acc_bits >= 0:
-                self.append_to_output(uncompressed, self.unk_table[(self.esi >> 0x18)]) # read value of unk_table from instream lookup
-                self.read_next(buffer)                          # read 2 bytes
-                self.esi = self.accumulator << (0x10 - self.available_acc_bits) # advance by 16 minus available bits
-                continue                                        # skip the rest and restart the loop
+                self.append_to_output(uncompressed, self.unk_table[(self.esi >> 0x18)])
+                self.read_next(buffer)
+                self.esi = self.accumulator << (0x10 - self.available_acc_bits)
+                continue
             self.available_acc_bits += self.eax - 0x10
-            if self.eax == 0x60:                                # 96 = specific break code?
+            if self.eax == 0x60:
                 self.eax = unk_9
-            else:                                               # how to handle long vals >8 bits.  Compare against the comparator values from loop 1
+            else:
                 self.eax = 8
                 self.edx = self.esi >> 16
                 self.ecx = 0x20
